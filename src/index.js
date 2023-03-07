99d6c4b003c9d9391f3e3956d6ebed96275f30be
muhammadv-i <mohamed10.sw2021@fci.helwan.edu.eg>
2023-03-07 17:31:14 +0000

    added (wip) editable fields and improved the css layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { format, formatDistance } from 'date-fns';
+
 const sidePanel = document.querySelector('.sidePanel');
 const mainPanel = document.querySelector('.mainPanel');
 
@@ -7,8 +9,9 @@ function todo(title, description, dueDate, priority, notes, project = 'main') {
         _dueDate = dueDate,
         _priority = priority,
         _notes = notes,
-        _project = project;
-        
+        _project = project,
+        _createdOn = new Date();
+
         function setTitle(newTitle) { _title = newTitle; }
         function getTitle() { return _title; }
     
@@ -26,6 +29,8 @@ function todo(title, description, dueDate, priority, notes, project = 'main') {
         
         function setProject(newProj) { _project = newProj; }
         function getProject() { return _project; }
+
+        function getCreatedOn() { return _createdOn; }
         
         let obj = {
             setTitle, 
@@ -40,6 +45,7 @@ function todo(title, description, dueDate, priority, notes, project = 'main') {
             getNotes,
             setProject,
             getProject,
+            getCreatedOn,
         };
 
         displayTodo(obj);
@@ -51,6 +57,7 @@ function displayTodo(todo) {
     let element = document.createElement('div');
     element.classList.add('todo');
     element.dataset.project = todo.getProject();
+    element.dataset.priority = todo.getPriority();
 
     let title = document.createElement('p');
     title.classList.add('title')
@@ -62,11 +69,11 @@ function displayTodo(todo) {
 
     let dueDate = document.createElement('p');
     dueDate.classList.add('dueDate')
-    dueDate.innerText = todo.getDueDate();
+    dueDate.innerText = `Due on ${format(todo.getDueDate(), 'dd/MM/yyyy')}`;
 
     let priority = document.createElement('p');
     priority.classList.add('priority')
-    priority.innerText = todo.getPriority();
+    priority.innerText = element.dataset.priority;
 
     let notes = document.createElement('p');
     notes.classList.add('notes')
@@ -74,14 +81,68 @@ function displayTodo(todo) {
 
     let project = document.createElement('p');
     project.classList.add('project')
-    project.innerText = todo.getProject();
+    project.innerText = element.dataset.project;
 
+    let createdOn = document.createElement('p');
+    createdOn.innerText = formatDistance(todo.getCreatedOn(), new Date(), { addSuffix: true });
+    createdOn.classList.add('createdOn');
+    
 
-    element.append(title, description, dueDate, priority, notes, project);
+    element.append(title, description, dueDate, priority, notes, project, createdOn);
+    Array.from(element.children).forEach( child => edit(child));
+
+    let g1 = document.createElement('div');
+    g1.classList.add('top-line');
+
+    let subg1 = document.createElement('div');
+    subg1.append(title, project)
+    let subg2 = document.createElement('div');
+    subg2.append(priority, createdOn);
+
+    g1.append(subg1, subg2);
+
+    element.insertBefore(g1, element.children[0]);
+    
 
     mainPanel.appendChild(element);
 }
 
-todo('asasd', 'ads', new Date(), '132', '214124ass asdasd');
+function edit(field) {
+    let parent = field.parentElement;
+    field.dataset.isEditing = 'false';
+
+    field.addEventListener("click", (e) => {
+        if (field.dataset.isEditing === 'false') {
+            field.dataset.isEditing = 'true';
+            let editor;
+            
+            if (field.classList[0] === 'description') {
+                editor = document.createElement('textarea');
+            }
+            else {
+                editor = document.createElement('input');
+                editor.type = 'text'; 
+            }
+
+            editor.value = field.innerText;
+            editor.classList.add(...field.classList);
+            
+            field.classList.toggle('hidden');
+            parent.insertBefore(editor, field);
+
+            editor.addEventListener('keydown', (e) => {
+                if (e.code === 'Enter') {
+                    field.innerText = editor.value;
+                    field.classList.toggle('hidden'); 
+                    field.dataset.isEditing = 'false';
+                    editor.remove();
+                }
+            })
+        }
+    })
+
+}
+
+todo('Date-fns', `The characters wrapped between two single quotes characters (') are escaped. Two single quotes in a row, whether inside or outside a quoted sequence, represent a 'real' single quote. (see the last example)`, new Date(2026, 1, 1), 'High', '214124ass asdasd');
 todo('title', 'description', new Date(), 'priority', 'notes');
 todo('title', 'description', new Date(), 'priority', 'notes');
